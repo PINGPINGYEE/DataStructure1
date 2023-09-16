36f833ee08c74f67499dc62643b28156b4642668
PINGPINGYEE <owb7994@nate.com>
2023-09-16 23:46:16 +0900

    Add files via upload

---
+++ SumPractice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int sum = 0;
+	int i = 2;
+	int j;
+	int cnt = 0;
+
+	while (i < 101)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				cnt += 1;
+			}
+		}
+		if (cnt == 0)
+		{
+			sum += i;
+		}
+		cnt = 0;
+		i++;
+	}
+	printf("%d", sum);
+}
